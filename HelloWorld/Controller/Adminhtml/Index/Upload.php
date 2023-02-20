cf316c6760bf82a33355c3b1508bde4d2e931b45
usol <ayeshasadiqa06@gmail.com>
2023-02-20 15:31:52 +0500

    Last commit wasn't full.In this commit all five fileds 1-massaction and 2 actions  ,are working.Service contracts not implemented.

---
+++ HelloWorld/Controller/Adminhtml/Index/Upload.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Devhooks\HelloWorld\Controller\Adminhtml\Index;
+
+use Magento\Framework\Controller\ResultFactory;
+
+class Upload extends \Magento\Backend\App\Action
+{
+
+    /**Devhooks\HelloWorld\Model\ImageUploader
+     */
+    public $imageUploader;
+
+    /**
+     * Upload constructor.
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param Devhooks\HelloWorld\Model\ImageUploader $imageUploader
+     */
+	public function __construct(
+		\Magento\Backend\App\Action\Context $context,
+		\Devhooks\HelloWorld\Model\ImageUploader $imageUploader
+	) {
+		parent::__construct($context);
+		$this->imageUploader = $imageUploader;
+	}
+
+    /**
+     * @return mixed
+     */
+	public function _isAllowed() {
+		return $this->_authorization->isAllowed('Devhooks_HelloWorld::upload');
+	}
+
+    /**
+     * @return mixed
+     */
+    public function execute() {
+		try {
+			$result = $this->imageUploader->saveFileToTmpDir('image_field');
+			$result['cookie'] = [
+				'name' => $this->_getSession()->getName(),
+				'value' => $this->_getSession()->getSessionId(),
+				'lifetime' => $this->_getSession()->getCookieLifetime(),
+				'path' => $this->_getSession()->getCookiePath(),
+				'domain' => $this->_getSession()->getCookieDomain(),
+			];
+		} catch (\Exception $e) {
+			$result = ['error' => $e->getMessage(), 'errorcode' => $e->getCode()];
+		}
+		return $this->resultFactory->create(ResultFactory::TYPE_JSON)->setData($result);
+	}
+}
